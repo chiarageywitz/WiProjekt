4d156f9d90991468922a405c53128122dcdc45ee
Ngoc Bao Chi Phan <Ngoc Bao Chi Phan@Bao_Chi.fritz.box>
2025-12-08 17:41:45 +0100

    Datenbank funktioniert nicht :(

--- src/Datenbank/DB.java
+++ src/Datenbank/DB.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class TestDB {
+public class DB {
 	
 	public static void main(String[] args) {
 		System.out.println("MySQL Connect Example V2 AWS");
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module WiProject {
+	requires mysql.connector.java;
 }
