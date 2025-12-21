8d37c0466e2d36f61603f3200a224352e614e01f
chiarageywitz <chiara.geywitz@gmail.com>
2025-12-21 22:38:27 +0100

    JavaDoc StartPortal

--- src/GUIKlassen/StartPortal.java
+++ src/GUIKlassen/StartPortal.java
@@ -2,12 +2,23 @@ package GUIKlassen;
 
 import javax.swing.SwingUtilities;
 
+/**
+ * Startklasse des Portals. Initialisiert das Login-Fenster beim Start der
+ * Anwendung.
+ * 
+ */
 public class StartPortal {
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            LoginFenster login = new LoginFenster();
-            login.setVisible(true);
-        });
-    }
+	/**
+	 * Main-Methode zum Starten des Hochschulportals.
+	 * Startet die GUI im Event-Dispatch-Thread und zeigt das Login-Fenster an.
+	 * 
+	 * @param args Kommandozeilenargumente (werden nicht verwendet)
+	 */
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			LoginFenster login = new LoginFenster();
+			login.setVisible(true);
+		});
+	}
 }
