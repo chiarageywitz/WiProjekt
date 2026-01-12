4645c8ffa2924ac46fa0e9e020795f9988b1e1b1
chiarageywitz <chiara.geywitz@gmail.com>
2026-01-12 21:57:36 +0100

    Java Doc

--- src/Fachobjekte/Studiendekan.java
+++ src/Fachobjekte/Studiendekan.java
@@ -1,28 +1,58 @@
 package Fachobjekte;
 
+/**
+ * Repräsentiert einen Studiendekan mit Benutzername und Passwort
+ */
 public class Studiendekan {
-	private String benutzername;
-	private String passwort;
 
-	public Studiendekan(String benutzername, String passwort) {
-		this.benutzername = benutzername;
-		this.passwort = passwort;
-	}
+    private String benutzername;
+    private String passwort;
 
-	public String getBenutzername() {
-		return benutzername;
-	}
+    /**
+     * Konstruktor
+     * Erstellt einen Studiendekan mit Benutzername und Passwort
+     *
+     * @param benutzername Name des Benutzers
+     * @param passwort Passwort des Benutzers
+     */
+    public Studiendekan(String benutzername, String passwort) {
+        this.benutzername = benutzername;
+        this.passwort = passwort;
+    }
 
-	public void setBenutzername(String benutzername) {
-		this.benutzername = benutzername;
-	}
+    /**
+     * Gibt den Benutzernamen zurück
+     *
+     * @return Benutzername des Studiendekans
+     */
+    public String getBenutzername() {
+        return benutzername;
+    }
 
-	public String getPasswort() {
-		return passwort;
-	}
+    /**
+     * Setzt den Benutzernamen
+     *
+     * @param benutzername Neuer Benutzername
+     */
+    public void setBenutzername(String benutzername) {
+        this.benutzername = benutzername;
+    }
 
-	public void setPasswort(String passwort) {
-		this.passwort = passwort;
+    /**
+     * Gibt das Passwort zurück
+     *
+     * @return Passwort des Studiendekans
+     */
+    public String getPasswort() {
+        return passwort;
+    }
 
-	}
+    /**
+     * Setzt das Passwort
+     *
+     * @param passwort Neues Passwort
+     */
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
 }
