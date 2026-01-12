f04f176dbce05776c256dc9b354b3349479532ec
chiarageywitz <chiara.geywitz@gmail.com>
2026-01-12 21:56:28 +0100

    Java Doc

--- src/Fachobjekte/Student.java
+++ src/Fachobjekte/Student.java
@@ -1,37 +1,78 @@
 package Fachobjekte;
+
+/**
+ * Repräsentiert einen Studenten mit Benutzername, Passwort und Matrikelnummer.
+ */
 public class Student {
 
-	private String benutzername;
-	private String passwort;
-	private String matrikelnummer; // Beispiel zusätzliches Attribut
+    private String benutzername;
+    private String passwort;
+    private String matrikelnummer; // Beispiel zusätzliches Attribut
 
-	public Student(String benutzername, String passwort, String matrikelnummer) {
-		this.benutzername = benutzername;
-		this.passwort = passwort;
-		this.matrikelnummer = matrikelnummer;
-	}
+    /**
+     * Konstruktor zur Initialisierung eines Studenten.
+     *
+     * @param benutzername Benutzername des Studenten
+     * @param passwort     Passwort des Studenten
+     * @param matrikelnummer Matrikelnummer des Studenten
+     */
+    public Student(String benutzername, String passwort, String matrikelnummer) {
+        this.benutzername = benutzername;
+        this.passwort = passwort;
+        this.matrikelnummer = matrikelnummer;
+    }
 
-	public String getBenutzername() {
-		return benutzername;
-	}
+    /**
+     * Gibt den Benutzernamen des Studenten zurück.
+     *
+     * @return Benutzername
+     */
+    public String getBenutzername() {
+        return benutzername;
+    }
 
-	public void setBenutzername(String benutzername) {
-		this.benutzername = benutzername;
-	}
+    /**
+     * Setzt den Benutzernamen des Studenten.
+     *
+     * @param benutzername Neuer Benutzername
+     */
+    public void setBenutzername(String benutzername) {
+        this.benutzername = benutzername;
+    }
 
-	public String getPasswort() {
-		return passwort;
-	}
+    /**
+     * Gibt das Passwort des Studenten zurück.
+     *
+     * @return Passwort
+     */
+    public String getPasswort() {
+        return passwort;
+    }
 
-	public void setPasswort(String passwort) {
-		this.passwort = passwort;
-	}
+    /**
+     * Setzt das Passwort des Studenten.
+     *
+     * @param passwort Neues Passwort
+     */
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
 
-	public String getMatrikelnummer() {
-		return matrikelnummer;
-	}
+    /**
+     * Gibt die Matrikelnummer des Studenten zurück.
+     *
+     * @return Matrikelnummer
+     */
+    public String getMatrikelnummer() {
+        return matrikelnummer;
+    }
 
-	public void setMatrikelnummer(String matrikelnummer) {
-		this.matrikelnummer = matrikelnummer;
-	}
+    /**
+     * Setzt die Matrikelnummer des Studenten.
+     *
+     * @param matrikelnummer Neue Matrikelnummer
+     */
+    public void setMatrikelnummer(String matrikelnummer) {
+        this.matrikelnummer = matrikelnummer;
+    }
 }
